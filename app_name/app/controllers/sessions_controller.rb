906c3a4c0ccfad9300f416cf76f75ef42544454f
kevin lee <kl2695@nyu.edu>
2017-10-31 21:06:11 -0700

    Add rest of w4 homeworks

---
+++ app_name/app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+
+
+
+  def create
+   user = User.find_by_credentials(
+     params[:user][:username],
+     params[:user][:password]
+   )
+
+   if user.nil?
+     render json: 'Credentials were wrong'
+   else
+     redirect_to user_url(user)
+   end
+
+
+ end
+
+ def new
+ end
+end
